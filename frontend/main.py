6b525c5f68bb6f8893407e0122a314a6f6f0350b
Paul Bricman <paulbricman@protonmail.com>
2022-02-09 13:03:40 +0100

    feat: tweakable layout

--- frontend/main.py
+++ frontend/main.py
@@ -1,26 +1,51 @@
+import importlib
 import streamlit as st
-from components import header, inspector, navigator, viewport, ranker
+from components import header, viewport
 from components import microverses
+import json
 
 
 st.set_page_config(
     page_title='💡 conceptarium',
     layout='wide')
 
-
 microverses.paint()
 header.paint()
 
-cols = st.columns(5)
+params = st.experimental_get_query_params()
+layout = params.get('layout')
 
-for component in [navigator, ranker]:
+if not layout:
+    layout = {
+        'leftColumn': ['navigator', 'ranker'],
+        'rightColumn': ['inspector'],
+        'viewportCols': 3
+    }
+else:
+    layout = json.loads(params['layout'][0])
+    if 'viewportCols' not in layout.keys():
+        layout['viewportCols'] = 3
+    if 'leftColumn' not in layout.keys():
+        layout['leftColumn'] = ['navigator', 'ranker']
+    if 'rightColumn' not in layout.keys():
+        layout['rightColumn'] = ['inspector']
+
+col_count = layout['viewportCols'] + \
+    int(len(layout['leftColumn']) > 0) + int(len(layout['rightColumn']) > 0)
+cols = st.columns(col_count)
+
+for component in layout['leftColumn']:
     with cols[0]:
-        with st.expander(component.get_name(), True):
-            component.paint()
+        m = importlib.import_module('components.' + component)
+        with st.expander(m.get_name(), True):
+            m.paint()
 
-for component in [inspector]:
+for component in layout['rightColumn']:
     with cols[-1]:
-        with st.expander(component.get_name(), True):
-            component.paint()
+        m = importlib.import_module('components.' + component)
+        with st.expander(m.get_name(), True):
+            m.paint()
 
-viewport.paint(cols[1:-1])
+start_viewport_col = int(len(layout['leftColumn']) > 0)
+end_viewport_col = start_viewport_col + layout['viewportCols']
+viewport.paint(cols[start_viewport_col:end_viewport_col])
